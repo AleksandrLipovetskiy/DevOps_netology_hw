6589c61b8fe5c24131742c2020f773b2fbc4ba98
AleksandrLipovetskiy <stsledov@gmail.com>
2025-07-26 10:40:33 +0300

    исправил 1 ошибку при terraform validate

--- 17_Ansible/17_3_Ansible_YC/src1/ansible.tf
+++ 17_Ansible/17_3_Ansible_YC/src1/ansible.tf
@@ -14,11 +14,13 @@ resource "local_file" "ansible_inventory" {
         fqdn = "${vm.name}.${var.vpc_name}.internal"
       }
     ]
-    mon_vms = {
-      name = yandex_compute_instance.mon.name
-      network_interface = yandex_compute_instance.mon.network_interface
-      fqdn = "${yandex_compute_instance.mon.name}.${var.vpc_name}.internal"
-    }
+    mon_vms = [
+      for vm in yandex_compute_instance.mon : {
+        name = vm.name
+        network_interface = vm.network_interface
+        fqdn = "${vm.name}.${var.vpc_name}.internal"
+      }
+    ]
   })
   filename = "${path.module}/inventory.yml"
 }
