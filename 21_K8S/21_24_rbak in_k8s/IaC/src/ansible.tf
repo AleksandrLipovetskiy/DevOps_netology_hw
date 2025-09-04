e6e2d585797b084710c007c3c9a0853bafd52981
AleksandrLipovetskiy <stsledov@gmail.com>
2025-09-04 22:25:01 +0300

    Задание «Управление доступом»

---
+++ 21_K8S/21_24_rbak in_k8s/IaC/src/ansible.tf
@@ -0,0 +1,20 @@
+locals {
+  ansible_user = "ubuntu"
+  ansible_private_key_file = "/home/sam/.ssh/id_ed25519"
+}
+
+resource "local_file" "ansible_inventory" {
+  content = templatefile("${path.module}/inventory.tftpl", {
+    web_vms = [
+      for vm in yandex_compute_instance.web : {
+        name = vm.name
+        network_interface = vm.network_interface
+        fqdn = "${vm.name}.${var.vpc_name}.internal"
+      }
+    ]
+        
+    ansible_user             = local.ansible_user
+    ansible_private_key_file = local.ansible_private_key_file
+  })
+  filename = "${path.module}/../playbook/inventory/prod.yml"
+}
